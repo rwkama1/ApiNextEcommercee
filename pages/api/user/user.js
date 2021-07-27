bfeb37c736298d3d931d57c242042c5595b450c3
rwkama1 <rwkamandriw@gmail.com>
2021-07-26 22:39:17 -0300

    login

---
+++ pages/api/user/user.js
@@ -0,0 +1,35 @@
+import cors from "../cors";
+import { FactoryLogic } from "e-commercee/ECommerce/dist/logic/FactoryLogic";
+import { Administrator } from "e-commercee/ECommerce/dist/shared/entity/Administrator";
+
+export default async function (req, res) 
+{
+  if(req.method==="GET")
+  {
+    try
+    {
+        await cors(req, res)
+        const {pidcard} = req.query;
+        const getadmin=await FactoryLogic.getLUser().getUser(pidcard);
+        return res.send(getadmin);
+    }
+    catch (error) {
+      return res.status(500).send("Could not search admin "+error.message);
+    }
+  }
+  if(req.method==="POST")
+  {
+    await cors(req, res)
+    try
+    {
+     const {pusername,ppassword} = req.query;
+        const getadmin=await FactoryLogic.getLUser().loginUser(pusername,ppassword);
+        return res.send(getadmin); 
+    }
+    catch (error) {
+      return res.status(500).send("Could not login admin: "+error.message);    
+      
+    }
+  }
+
+}
