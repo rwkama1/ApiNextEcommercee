e898ccd4c23ec6a79a24d8b656457b157dc50ebb
rwkama1 <rwkamandriw@gmail.com>
2021-08-06 22:46:53 -0300

    removeitemonorder

---
+++ pages/api/order/post/removeitemonorder.js
@@ -0,0 +1,20 @@
+import cors from "../../cors";
+import { FactoryLogic } from "e-commercee/ECommerce/dist/logic/FactoryLogic";
+
+
+export default async function (req, res) 
+{
+ if(req.method==="POST")
+  {
+    try
+    {
+        await cors(req, res)
+        const data = req.body;
+        const removeitem=await FactoryLogic.getLOrder().removeItemonOrder(data.barcode);
+        return res.send(removeitem);
+    }
+    catch (error) {
+      return res.status(500).send("Could not remove item  order "+error.message);
+    }
+  }
+}
